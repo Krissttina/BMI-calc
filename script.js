7df033320304c62e354cd37d9fa538c099b80894
Kristina <krismetodieva123@gmail.com>
2023-05-02 19:34:32 +0300

    update
    calculating height/weight

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let prize = document.querySelector('.prize');
-let tip = document.querySelector('.money-given');
-let output = document.querySelector('.tip');
-let button = document.querySelector('.button');
+const radioMale = document.querySelector(".male");
+const radioFemale = document.querySelector(".female");
 
-button.addEventListener('click', function(){
-    let giveMoney = Math.abs(prize - tip);
-    output.innerHTML = giveMoney;
-})
+const height = document.querySelector(".height");
+const weight = document.querySelector(".weight");
+
+const result = document.querySelector(".result");
+
+document.getElementById("btn").addEventListener("click", () => {
+    let bmi = Number(height.value) / Number(weight.value);
+
+    result.innerHTML = `Result: ${bmi.toFixed(2)}`
+
+  if (radioFemale.checked == true) {
+    console.log(bmi);
+  } else {
+    console.log(bmi);
+  }
+});
+
+// divides an adult's weight in kilograms (kg) 
+//by their height in metres (m) squared
